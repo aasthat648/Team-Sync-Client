5601bb1fb044a1d1b657171f390e9e90053c7871
aasthat648 <aasthat648@gmail.com>
2026-01-07 22:47:05 +0530

    landing page created

--- src/app/pages/client/home/home.ts
+++ src/app/pages/client/home/home.ts
@@ -1,11 +1,25 @@
-import { ThemeComponent } from '@/shared/components/theme/theme';
+import { ZardIconComponent } from '@/shared/components/icon/icon.component';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ThemeComponent, RouterLink],
+  imports: [RouterLink, ZardIconComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  year = new Date().getFullYear();
+  openIndex = -1;
+  setOpen(index: number, event: Event) {
+    const target = event.target as HTMLDetailsElement;
+    if (target.open) {
+      this.openIndex = index;
+    } else {
+      if (this.openIndex === index) {
+        this.openIndex = -1;
+      }
+    }
+  }
+
+}
