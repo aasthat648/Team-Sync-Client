da2ad10ffa3aff57c1762c06ece173b3f507dc17
Ishika Patel <ishikapatel1364@gmail.com>
2026-01-23 22:18:15 +0530

    refactor settings and tasks components for improved layout and accessibility

--- src/app/pages/client/dashboard/settings/settings.ts
+++ src/app/pages/client/dashboard/settings/settings.ts
@@ -1,11 +1,12 @@
+import { ZardAvatarComponent } from '@/shared/components/avatar/avatar.component';
 import { Component } from '@angular/core';
+import { ZardInputDirective } from '@/shared/components/input/input.directive';
+import { ZardButtonComponent } from '@/shared/components/button/button.component';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [ZardAvatarComponent, ZardInputDirective, ZardButtonComponent],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {
-
-}
+export class Settings {}
