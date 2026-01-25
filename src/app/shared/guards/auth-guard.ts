f77c52a5a8c71dac1d971b951c16b66b5b1be88d
aasthat648 <aasthat648@gmail.com>
2026-01-25 18:31:25 +0530

    environment file updated

--- src/app/shared/guards/auth-guard.ts
+++ src/app/shared/guards/auth-guard.ts
@@ -1,3 +1,4 @@
+import { UserStore } from '@/stores/user.store';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
@@ -5,8 +6,11 @@ export const authGuard: CanActivateFn = (route, state) => {
   if (typeof window === 'undefined') {
     return true;
   }
+  const userStore = inject(UserStore);
   const router = inject(Router);
+  // const user = userStore.user();
   const token = localStorage.getItem('token');
+
   if (!token) {
     router.navigate(['/login']);
     return false;
