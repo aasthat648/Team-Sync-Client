b66d65b7e1c58ce5e64b1e40ef036b3a57c4b35a
aasthat648 <aasthat648@gmail.com>
2026-02-09 15:17:40 +0530

    project api done

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -5,6 +5,7 @@ import { ZardIconComponent } from '@/shared/components/icon/icon.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { CreateProject } from '@/shared/custom-components/dashboard/create-project/create-project';
+import { AuthStore } from '@/store/auth';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,15 +15,18 @@ import { CreateProject } from '@/shared/custom-components/dashboard/create-proje
 export class Dashboard {
   private dialogService = inject(ZardDialogService);
   private router = inject(Router);
+  private authStore = inject(AuthStore);
   readonly activeTab = signal<'projects' | 'tasks' | 'members'>('projects');
 
   openProject() {
     console.log('Opening Create Project Dialog');
+    const workspaceId = this.authStore.snapshot?.currentWorkspace ?? null;
     this.dialogService.create({
       zTitle: 'Create Project',
       zDescription: 'Create your own project',
       zContent: CreateProject,
       zWidth: '425px',
+      zData: { workspaceId },
       zOkText: null,
       zCancelText: null,
       zClosable: true,
